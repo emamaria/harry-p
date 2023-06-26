6703565a55e2961ee1c319e15c036fdaac5171db
emamaria <emascript@gmail.com>
2023-06-26 19:03:43 +0200

    characterDetail page style

---
+++ src/components/CharacterDetail.vue
@@ -0,0 +1,104 @@
+<template>
+  
+  <main :class="charData.color">
+ 
+    <div class="img_contanier" >
+        
+    <img :src="charData.image" :alt="charData.name">   
+    </div>
+
+    <div class="character_data_container">
+    <p>Species: &nbsp;{{charData.species}}</p>
+    <p>Gender: &nbsp;{{charData.gender}}</p>
+    <p>House:&nbsp;{{charData.house}}</p>
+     <p>Date of Birth:&nbsp;{{charData.dateOfBirth}}</p>
+     <p>Year of Birth:&nbsp;{{charData.yearOfBirth}}</p>
+     <p>Wizard:&nbsp;{{charData.wizard}}</p>
+     <p>Ancestry: &nbsp;{{charData.ancestry}}</p>
+     <p>Eye Colour:&nbsp; {{charData.eyeColour}}</p>
+     <p>Hair Colour:&nbsp;{{charData.hairColour}}</p>
+     <p>Patronus:&nbsp;{{charData.patronus}}</p>
+     <p>Hogwarts Student:&nbsp;{{charData.hogwartsStudent}}</p>
+     <p>Hogwarts Staff:&nbsp; {{charData.hogwartsStaff}}</p>
+     <p>Actor:&nbsp;{{charData.actor}}</p>
+     <p>Alive:&nbsp;{{charData.alive}}</p>
+     <ul>
+        <h4>Wand:</h4>
+        <li>Wood:&nbsp;{{charData.wand.wood}} </li>
+        <li>Core:&nbsp;{{charData.wand.core}} </li>
+        <li>Length:&nbsp;{{charData.wand.length}}</li>
+     </ul>
+    </div>
+   
+  </main>
+</template>
+
+<script>
+export default {
+
+   props:{
+     charData: {
+        required: true,
+        type: Object
+     }
+   }
+}
+</script>
+
+<style scoped>
+ p, li{
+    margin: 5px;
+ }
+
+ h4{
+    margin: 5px;
+ }
+
+
+ main{
+    display: flex;
+    flex-direction: row;
+    flex-wrap:wrap;
+    min-width:300px;
+    height: 600px;
+    margin: 50px;
+    padding: 2rem;
+    justify-content: center;
+ }
+
+ 
+
+ .img_container{
+    background-color: pink;
+   
+ }
+
+ img{
+    object-fit: contain;
+    width: 270px;
+    height: 350px;
+ }
+
+ .character_data_container{
+    display: flex;
+    flex-direction: column;
+ 
+ }
+
+ .red{
+    background-color:#a6332e;
+  }
+
+  .blue{
+    background-color: #3c4e91;
+  }
+
+  .green{
+   background-color:#366447;
+  }
+
+  .yellow{
+    background-color:#efbc2f;
+  }
+
+</style>
--- src/pages/Character.vue
+++ src/pages/Character.vue
@@ -1,6 +1,10 @@
 <template>
 <main>
- <p v-if="characterData">{{characterData.name}}</p>
+  <div class="character_detail_container">
+    <h3>{{characterData.name}}</h3>
+     <CharacterDetail v-if="characterData"  :char-data="characterData"/>
+  </div>
+
 </main>
  
 </template>
@@ -8,9 +12,14 @@
 <script>
 
 import {getApiData} from '../api/getApiData'
-
+import {getCharacterColor} from '../helpers/getCharacterColor'
+import { defineAsyncComponent } from 'vue'
 export default {
 
+   components:{
+     
+      CharacterDetail: defineAsyncComponent(() => import('../components/CharacterDetail.vue'))
+    },
    props: {
       id:{
         required: true,
@@ -28,7 +37,11 @@ export default {
        this.characterData = charactersArray.slice(0,25).filter(character => character.id === this.id)
                                           .map( data => {
            const {id, name,species, gender, house, dateOfBirth,yearOfBirth, wizard, ancestry, eyeColour, hairColour,wand, patronus,hogwartsStudent, hogwartsStaff, actor,alive,image} = data
-                      return {
+                     
+
+                     let color = getCharacterColor(house.toLowerCase())
+
+                     return {
                        id,
                        name,
                        species,
@@ -45,7 +58,8 @@ export default {
                        hogwartsStaff,
                        actor,
                        alive,
-                       image
+                       image,
+                       color
                        }
                     })[0]
 
@@ -60,4 +74,34 @@ main{
      flex-direction: column;
      min-height: 100vh;    
 }
+
+.character_detail_container{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  align-items: center;
+}
+
+h3{
+  margin-top: 3rem;
+}
+
+.red{
+    background-color:#a6332e;
+  }
+
+  .blue{
+    background-color: #3c4e91;
+  }
+
+  .green{
+   background-color:#366447;
+  }
+
+  .yellow{
+    background-color:#efbc2f;
+  }
+
+
 </style>
