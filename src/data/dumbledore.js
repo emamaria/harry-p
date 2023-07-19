03b0f7e3a3aedc546f44cb4c22186387a9cfcab4
emamaria <emascript@gmail.com>
2023-07-19 13:41:48 +0200

    reduce text

--- src/data/dumbledore.js
+++ src/data/dumbledore.js
@@ -3,8 +3,7 @@ const dumbledoreData = {
 name: "Albus Dumbledore",
 description:`He is the headmaster of Hogwarts and he is the founder and leader of the
 Order of the Phoenix, an organisation dedicated to fighting Lord Voldemort.
-He is a mentor of Harry Potter.Dumbledore is a very wise man who knows
-that Harry is going to have to learn a few hard lessons to prepare him for
+He is a mentor of Harry Potter.Dumbledore knows that Harry is going to have to learn hard things to prepare him for
 what may be coming in his life. He allows Harry to get into what he
 wouldn't allow another pupil to do, and he also unwillingly permits Harry
 to confront things he’d rather protect him from.`
--- src/data/hogwarts.js
+++ src/data/hogwarts.js
@@ -4,9 +4,7 @@ magic for students aged eleven to eighteen.Founded in the 10th
 century by Godric Gryffindor, Rowena Ravenclaw, Helga Hufflepuff and
 Salazar Slytherin, Hogwarts was established in the Highlands of Scotland
 to educate young wizards and witches as well as to keep students safe from
-muggle persecution.Hogwarts educated most wizarding children in the
-United Kingdom and its surrounding areas, keeping its location hidden from
-other wizarding schools and muggles.
+muggle persecution.Hogwarts location is hidden from other wizarding schools and muggles.
 `
 
 
